e30254ed31a3965e2425c55b5a930a9b57c2fa64
vikramg <vikramg@igcar.gov.in>
2025-03-14 17:11:23 +0000

    add path and play options

--- main.py
+++ main.py
@@ -6,6 +6,8 @@
 # tinytag library
 from tinytag import TinyTag
   
+path = "/mnt/c/Users/DELL/Music/tamil mix/Jamal Kudu_128-(DJPunjab).mp3"
+
 # Pass the filename into the
 # Tinytag.get() method and store
 # the result in audio variable
@@ -23,3 +25,7 @@ print("Filesize: " + str(audio.filesize) + " bytes")
 print("AlbumArtist: " + audio.albumartist)
 print("Duration: " + str(audio.duration) + " seconds")
 print("TrackTotal: " + str(audio.track_total))
+
+import vlc
+p = vlc.MediaPlayer(path)
+p.play()
